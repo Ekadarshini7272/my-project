580f79c84068b1b13e68cfb169b90320eb64ba7a
Ekadarshini7272 <routrayekadarshini@gmail.com>
2024-03-07 14:38:36 +0530

    Initial commit for frontend

--- backend/models/Data.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dataSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  executionTime: Number,
-});
-
-module.exports = mongoose.model('Data', dataSchema);
